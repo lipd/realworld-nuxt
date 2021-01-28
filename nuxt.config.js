c473c01f8d2c2a6b6d1860cdf7e7961b3896e504
lipd <lipeidao@gmail.com>
2021-01-28 19:06:04 +0800

    fix:  correct layout

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,37 +15,37 @@ module.exports = {
                 name: 'home',
                 component: resolve(__dirname, 'pages/home/'),
               },
+              {
+                path: '/login',
+                name: 'login',
+                component: resolve(__dirname, 'pages/login/'),
+              },
+              {
+                path: '/register',
+                name: 'register',
+                component: resolve(__dirname, 'pages/login/'),
+              },
+              {
+                path: '/profile/:username',
+                name: 'profile',
+                component: resolve(__dirname, 'pages/profile/'),
+              },
+              {
+                path: '/settings',
+                name: 'settings',
+                component: resolve(__dirname, 'pages/settings/'),
+              },
+              {
+                path: '/editor',
+                name: 'editor',
+                component: resolve(__dirname, 'pages/editor/'),
+              },
+              {
+                path: '/article/:slug',
+                name: 'article',
+                component: resolve(__dirname, 'pages/article/'),
+              },
             ],
-          },
-          {
-            path: '/login',
-            name: 'login',
-            component: resolve(__dirname, 'pages/login/'),
-          },
-          {
-            path: '/register',
-            name: 'register',
-            component: resolve(__dirname, 'pages/login/'),
-          },
-          {
-            path: '/profile/:username',
-            name: 'profile',
-            component: resolve(__dirname, 'pages/profile/'),
-          },
-          {
-            path: '/settings',
-            name: 'settings',
-            component: resolve(__dirname, 'pages/settings/'),
-          },
-          {
-            path: '/editor',
-            name: 'editor',
-            component: resolve(__dirname, 'pages/editor/'),
-          },
-          {
-            path: '/article/:slug',
-            name: 'article',
-            component: resolve(__dirname, 'pages/article/'),
           },
         ],
       )
