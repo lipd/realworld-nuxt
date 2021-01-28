c046120ac428050981446796169518152633c071
lipd <lipeidao@gmail.com>
2021-01-28 18:27:16 +0800

    import rest pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,6 +27,26 @@ module.exports = {
             name: 'register',
             component: resolve(__dirname, 'pages/login/'),
           },
+          {
+            path: '/profile/:username',
+            name: 'profile',
+            component: resolve(__dirname, 'pages/profile/'),
+          },
+          {
+            path: '/settings',
+            name: 'settings',
+            component: resolve(__dirname, 'pages/settings/'),
+          },
+          {
+            path: '/editor',
+            name: 'editor',
+            component: resolve(__dirname, 'pages/editor/'),
+          },
+          {
+            path: '/article/:slug',
+            name: 'article',
+            component: resolve(__dirname, 'pages/article/'),
+          },
         ],
       )
     },
