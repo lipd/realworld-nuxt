d1fcf4d3ba09fd173a6573826d63cfc5b5494aaf
lipd <lipeidao@gmail.com>
2021-01-31 19:47:36 +0800

    display article meta data

--- pages/article/index.vue
+++ pages/article/index.vue
@@ -3,23 +3,7 @@
     <div class="banner">
       <div class="container">
         <h1>{{ article.title }}</h1>
-
-        <div class="article-meta">
-          <a href=""><img src="http://i.imgur.com/Qr71crq.jpg"/></a>
-          <div class="info">
-            <a href="" class="author">Eric Simons</a>
-            <span class="date">January 20th</span>
-          </div>
-          <button class="btn btn-sm btn-outline-secondary">
-            <i class="ion-plus-round"></i>
-            &nbsp; Follow Eric Simons <span class="counter">(10)</span>
-          </button>
-          &nbsp;&nbsp;
-          <button class="btn btn-sm btn-outline-primary">
-            <i class="ion-heart"></i>
-            &nbsp; Favorite Post <span class="counter">(29)</span>
-          </button>
-        </div>
+        <article-meta :article="article" />
       </div>
     </div>
 
@@ -31,23 +15,7 @@
       <hr />
 
       <div class="article-actions">
-        <div class="article-meta">
-          <a href="profile.html"><img src="http://i.imgur.com/Qr71crq.jpg"/></a>
-          <div class="info">
-            <a href="" class="author">Eric Simons</a>
-            <span class="date">January 20th</span>
-          </div>
-
-          <button class="btn btn-sm btn-outline-secondary">
-            <i class="ion-plus-round"></i>
-            &nbsp; Follow Eric Simons <span class="counter">(10)</span>
-          </button>
-          &nbsp;
-          <button class="btn btn-sm btn-outline-primary">
-            <i class="ion-heart"></i>
-            &nbsp; Favorite Post <span class="counter">(29)</span>
-          </button>
-        </div>
+        <article-meta :article="article" />
       </div>
 
       <div class="row">
@@ -123,10 +91,12 @@
 <script>
 import { getArticle } from '@/api/article'
 import MarkdownIt from 'markdown-it'
+import ArticleMeta from './components/article-meta.vue'
 
 const md = new MarkdownIt()
 
 export default {
+  components: { ArticleMeta },
   name: 'ArticleIndex',
   async asyncData({ params }) {
     const { data } = await getArticle(params.slug)
